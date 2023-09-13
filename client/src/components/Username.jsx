0092275b4179c355d025d019393e56bd780f6fb7
Seyam <seyam.hossain.118833@gmail.com>
2023-09-13 21:35:21 +0600

    username component done

--- client/src/components/Username.jsx
+++ client/src/components/Username.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import avatar from "../assets/profile.png";
+
+// import styles
+import styles from "../styles/Username.module.css";
 
 export default function Username() {
-    
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-3xl font-bold">Hello Again</h4>
+            <span className="py-3 text-1xl w-2/3 text-center text-gray-500">
+              Explore more by connecting with us
+            </span>
+          </div>
+          <form action="" className="py-1">
+            <div className="profile flex justify-center py-4">
+              <img src={avatar} alt="avatar" className={styles.profile_img} />
+            </div>
 
-    return (
-        <h1>
-            Username
-        </h1>
-    )
+            <div className="textbox flex flex-col items-center gap-6">
+              <input
+                type="text"
+                placeholder="Username"
+                className={styles.textbox}
+              />
+              <button className={styles.btn} type="submit">
+                Let's go
+              </button>
+            </div>
+
+            <div className="text-center py-4">
+              <span>
+                Not a member?{" "}
+                <Link className="text-red-500" to="/register">
+                  Register now
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+
+// tailwind default css file
 import "./index.css";
+
+// default app render
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
