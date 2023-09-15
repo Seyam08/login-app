91ed278f1b7db98379973850ad39f0c3bdafbc00
Seyam <seyam.hossain.118833@gmail.com>
2023-09-15 20:04:23 +0600

    Recovery Component done

--- client/src/components/Recovery.jsx
+++ client/src/components/Recovery.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import { Link } from "react-router-dom";
+// import styles
+import styles from "../styles/Username.module.css";
+// import helpers
 
-export default function Recovery() {
-    
+export default function Recover() {
+  return (
+    <div className="container mx-auto">
+      <Toaster position="top-right" reverseOrder={false}></Toaster>
 
-    return (
-        <h1>Recovery</h1>
-    )
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-3xl font-bold">Recovery</h4>
+            <span className="py-3 text-1xl w-2/3 text-center text-gray-500">
+              Enter your OTP to recover password
+            </span>
+          </div>
+
+          <form className="py-1">
+            <div className="textbox flex flex-col items-center gap-6">
+              <span className="py-1 text-sm text-left text-gray-500">
+                Enter 6 digit OTP sent to your email address.
+              </span>
+              <input type="text" placeholder="OTP" className={styles.textbox} />
+              <button className={styles.btn} type="submit">
+                Recover
+              </button>
+            </div>
+
+            <div className="text-center py-4">
+              <span>
+                Didn't get any code?{" "}
+                <Link className="text-red-500" to="/recovery">
+                  Resend OTP
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
