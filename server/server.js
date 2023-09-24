ba2cd21ca7eab06dc5ecb966ef267468c6f0dfe4
Seyam <seyam.hossain.118833@gmail.com>
2023-09-24 20:44:34 +0600

    router setup done

--- server/server.js
+++ server/server.js
@@ -1,38 +1,46 @@
-import cors from "cors";
-import express from "express";
-import { MongoMemoryServer } from "mongodb-memory-server";
-import mongoose from "mongoose";
-import morgan from "morgan";
+/* eslint-disable import/extensions */
+import cors from 'cors';
+import express from 'express';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import router from './router/route.js';
 
 const app = express();
 
-// server port 
+// server port
 const port = 8000;
 
-// database connection 
-const mongodb = await MongoMemoryServer.create()
-const getUri = mongodb.getUri()
+// database connection
 
-mongoose.connect(getUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(()=> console.log('Database connection successful'))
-.catch((err)=> console.log(err))
+async function connect() {
+    try {
+        const mongodb = await MongoMemoryServer.create();
+        const getUri = mongodb.getUri();
+        await mongoose.connect(getUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Database connection successful');
+    } catch (err) {
+        console.error(err);
+    }
+}
 
+connect();
 // middleware
-app.use(morgan("tiny"))
-app.use(express.json())
-app.use(cors())
-app.disable("x-powered-by")
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(cors());
+app.disable('x-powered-by');
+app.use('/api', router);
 
-
-// home get req 
-app.get("/" , (req, res)=>{
-    res.status(201).json("home get request")
-})
+// home get req
+app.get('/', (req, res) => {
+    res.status(201).send('home get request');
+});
 
 /** start server only when we have valid connection */
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server running on ${port} port`);
-})
+});
